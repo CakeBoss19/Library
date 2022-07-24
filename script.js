aed4e39251c0682791829045635b7c34bd940886
Steven <stevenp0294@gmail.com>
2022-07-24 08:51:40 -0400

    javascript

--- script.js
+++ script.js
@@ -7,14 +7,34 @@ function Book(title, author, genre, read){
   this.read = read;
 }
 
-function addBookToLibrary(book){
-  myLibrary.push(book);
+Book.prototype.bookReport = function(){
+  this.read = (this.read.toLowerCase() === 'yes') ? 'Have read.' : 'Not read yet!';
+  return `${this.title} by ${this.author}, a ${this.genre} book. ${this.read}`
+}
+
+function addBookToLibrary(newBook){
+  myLibrary.push(newBook);
   return
 }
 
-const book1 = new Book('name', 'person', 'history', 'yes');
+function removeBookFromLibrary(bookInfo){
+  myLibrary.pop(bookInfo);
+  return
+};
 
-Book.prototype.bookReport = function(){
-  this.read = (this.read.toLowerCase() === 'yes') ? 'Have read.' : ' Not read yet!';
-  return `${this.title} by ${this.author}, a ${this.genre} book. ${this.read}`
-}
+function displayBooks(){
+  for(let i = 0; i < myLibrary.length; i++){
+    myLibrary[i].bookReport();
+  };
+};
+
+let book1 = new Book('yep', 'that guy', 'spooky-scary', 'nopers');
+let book2 = new Book('yep', 'this guy', 'spooky-scary', 'nopers');
+let book3 = new Book('yep', 'that guy', 'spooky-history', 'nopers');
+let book4 = new Book('yep', 'that guy', 'spooky-scary', 'yes');
+
+addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
+addBookToLibrary(book4)
+console.log(displayBooks());
