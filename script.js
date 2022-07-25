9d52c48bdbd51921f6b5a507f603163b0975655e
Steven <stevenp0294@gmail.com>
2022-07-25 16:16:07 -0400

    JAVASCRIPT-BEGAN

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let myLibrary = [];
+const btn = document.getElementById('add-book');
+const library_grid_div = document.getElementById('grid-library');
+const myLibrary = [];
 
 function Book(title, author, genre, read){
   this.title = title,
@@ -9,23 +11,36 @@ function Book(title, author, genre, read){
 
 Book.prototype.bookReport = function(){
   this.read = (this.read.toLowerCase() === 'yes') ? 'Have read.' : 'Not read yet!';
-  return `${this.title} by ${this.author}, a ${this.genre} book. ${this.read}`
+  return `${this.title} by ${this.author}, a ${this.genre} book. ${this.read}`;
 }
 
+  // // Creates a new book-tile after form submission
+function buildBookTile(newBook){
+  let book = document.createElement('div');
+  library_grid_div.appendChild(book).classList = 'book-tile';
+  // ** I think I have to create the ability to create objects first, THEN try to plug in the information into HTML
+  
+
+}
+
+ // // Adds new Book object to myLibrary array
 function addBookToLibrary(newBook){
   myLibrary.push(newBook);
   return
 }
 
+ // // Removes Book object from myLibrary array
 function removeBookFromLibrary(bookInfo){
   myLibrary.pop(bookInfo);
   return
 };
 
+ // // Loops through myLibrary array and displays book-tiles
 function displayBooks(){
   for(let i = 0; i < myLibrary.length; i++){
     myLibrary[i].bookReport();
   };
+  
 };
 
 let book1 = new Book('yep', 'that guy', 'spooky-scary', 'nopers');
