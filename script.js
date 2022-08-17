aa94b10baa5135fdd906599e17796e68b5d9c7ff
Steven <stevenp0294@gmail.com>
2022-08-17 16:27:59 -0400

    Found a way to access input information from form. Form submit button with add book objects to array as expected HTML displays the correct information as well To work on: remove from list button, form validation

--- script.js
+++ script.js
@@ -1,50 +1,69 @@
-// const btn = document.getElementById('add-book');
-// const library_grid_div = document.getElementById('grid-library');
+// / Creates a book tile using objects' properties and values.
+function buildBookTile(data){
+  // variables must be INSIDE function to avoid book tiles overwriting eachother.
+  let grid = document.getElementById('grid-library')
+  let book = document.createElement('div');
+  let title = document.createElement('h2')
+  let author = document.createElement('p');
+  let pages = document.createElement('p');
+  let read = document.createElement('p');
+  let span1 = document.createElement('span');
+  let span2 = document.createElement('span');
+  let span3 = document.createElement('span');
+  let span4 = document.createElement('span')
+  let label = document.createElement('label');
+  let input = document.createElement('input');
+
+  grid.appendChild(book).classList = 'book-tile';
+  book.appendChild(title).classList = 'title';
+    title.textContent = data[1].value;
+  book.appendChild(author); 
+    author.textContent = 'By: ';
+  book.appendChild(pages);  
+    pages.textContent = 'Pages: ';
+  book.appendChild(read);
+    author.appendChild(span1).classList = 'author-span info-span';
+      span1.textContent = data[2].value;
+    pages.appendChild(span2).classList = 'pages-span info-span';
+      span2.textContent = data[3].value;
+    read.appendChild(span3);     
+      span3.textContent = 'Read? ';
+    read.appendChild(label).classList = 'switch';
+      label.appendChild(input).type = 'checkbox'; 
+      label.appendChild(span4).classList = 'slider';
+};
+
+const form = document.getElementById('form')
+const data = document.querySelector('#form').elements;
+const btn = document.querySelector('#add-book');
 const myLibrary = [];
 
-function Book(title, author, genre, read){
+function Book(title, author, pages){
   this.title = title,
   this.author = author,
-  this.genre = genre,
-  this.read = read;
+  this.pages = pages;
 }
-
-Book.prototype.bookReport = function(){
-  this.read = (this.read.toLowerCase() === 'yes') ? 'Have read.' : 'Not read yet!';
-  return `${this.title} by ${this.author}, a ${this.genre} book. ${this.read}`;
-}
-
 
  // // Adds new Book object to myLibrary array
-function addBookToLibrary(newBook){
-  myLibrary.push(newBook);
-  return
+ function addBookToLibrary(data){
+  myLibrary.push(new Book(data[1].value, data[2].value, data[3].value));
+  buildBookTile(data);
+  return;
 }
 
  // // Removes Book object from myLibrary array
-function removeBookFromLibrary(bookInfo){
+function removeBookFromLibrary(){
   myLibrary.pop(bookInfo);
   return
 };
 
- // // Loops through myLibrary array and displays book-tiles
 function displayBooks(){
-  for(let i = 0; i < (myLibrary.length); i++){
-    buildBookTile(myLibrary[i]);
-  };
-  return
+  myLibrary.forEach(buildBookTile);
+  return;
 };
-
-let book1 = new Book('yep', 'that guy', 'spooky-scary', 'nopers');
-let book2 = new Book('yep', 'this guy', 'spooky-scary', 'nopers');
-let book3 = new Book('yep', 'that guy', 'spooky-history', 'nopers');
-let book4 = new Book('yep', 'that guy', 'spooky-scary', 'yes');
-let book5 = new Book('nope', 'sandwi9ch', 'hamburgers', 'YES')
-
-addBookToLibrary(book1)
-addBookToLibrary(book2)
-addBookToLibrary(book3)
-addBookToLibrary(book4)
-addBookToLibrary(book5)
-
-displayBooks();
+  
+btn.addEventListener('click', () =>{
+  addBookToLibrary(data);
+});
+ // // Loops through myLibrary array and displays book-tiles
+myLibrary.forEach(() => console.log('hey'));
