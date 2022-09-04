89fc030e241baf4bf649db0ee004cd6e14a7afef
Steven <stevenp0294@gmail.com>
2022-09-04 00:33:51 -0400

    Moving over files finsihed. Folders in the right placces!

--- script.js
+++ script.js
@@ -1,69 +1,143 @@
-// / Creates a book tile using objects' properties and values.
-function buildBookTile(data){
-  // variables must be INSIDE function to avoid book tiles overwriting eachother.
-  let grid = document.getElementById('grid-library')
-  let book = document.createElement('div');
-  let title = document.createElement('h2')
-  let author = document.createElement('p');
-  let pages = document.createElement('p');
-  let read = document.createElement('p');
-  let span1 = document.createElement('span');
-  let span2 = document.createElement('span');
-  let span3 = document.createElement('span');
-  let span4 = document.createElement('span')
-  let label = document.createElement('label');
-  let input = document.createElement('input');
-
-  grid.appendChild(book).classList = 'book-tile';
-  book.appendChild(title).classList = 'title';
-    title.textContent = data[1].value;
-  book.appendChild(author); 
-    author.textContent = 'By: ';
-  book.appendChild(pages);  
-    pages.textContent = 'Pages: ';
-  book.appendChild(read);
-    author.appendChild(span1).classList = 'author-span info-span';
-      span1.textContent = data[2].value;
-    pages.appendChild(span2).classList = 'pages-span info-span';
-      span2.textContent = data[3].value;
-    read.appendChild(span3);     
-      span3.textContent = 'Read? ';
-    read.appendChild(label).classList = 'switch';
-      label.appendChild(input).type = 'checkbox'; 
-      label.appendChild(span4).classList = 'slider';
-};
-
-const form = document.getElementById('form')
-const data = document.querySelector('#form').elements;
-const btn = document.querySelector('#add-book');
+// Set Global Variables
+const form_container = document.querySelector('.form_container');
+const form = document.getElementById('form');
+const form_btn = document.querySelector('#form-btn');
+const submit_btn = document.querySelector('#submit');
+const cancel_btn = document.querySelector('#cancel-form');
+const input_focus = document.querySelector('input[id="title"]');
+const buttons = document.querySelectorAll('button');
+const table_grid = document.getElementById('table-grid');
 const myLibrary = [];
 
-function Book(title, author, pages){
+// Creates p elements within a div to display information
+function buildTile(obj){
+ let table_div = document.createElement('div');
+ let title = document.createElement('p');
+ let author = document.createElement('p');
+ let pageNum = document.createElement('p');
+ let read = document.createElement('p');
+ let omit = document.createElement('p');
+ table_div.append(title, author, pageNum, read, omit);
+ let nodes = table_div.childNodes; // creates a nodeList of each p element created and appended
+ giveValues(obj, nodes);
+ table_grid.appendChild(table_div).classList = 'table-div';
+};
+
+// Attaches the information from the object onto it's DOM element
+function giveValues(obj, arr){
+  for(i = 0; i < arr.length; i++){
+   let key = Object.keys(obj)[i];
+   let keyValue = Object.values(obj)[i];
+   arr[i].classList = `table table-${key}`; // Each created p tag gets a class referencing Object.property being displayed
+   arr[i].textContent = keyValue; // each p tag displays as textContent the properties: values;
+   if(key === undefined){
+    console.log(Object.keys(obj)[4]);
+    arr[i].classList = `table table-remove`;
+   };
+  }; return 
+};
+
+ // Book Object Contructor
+function Book(title, author, page, read){
   this.title = title,
   this.author = author,
-  this.pages = pages;
-}
-
- // // Adds new Book object to myLibrary array
- function addBookToLibrary(data){
-  myLibrary.push(new Book(data[1].value, data[2].value, data[3].value));
-  buildBookTile(data);
-  return;
-}
-
- // // Removes Book object from myLibrary array
-function removeBookFromLibrary(){
-  myLibrary.pop(bookInfo);
-  return
+  this.number = page,
+  this.read = read;
 };
 
-function displayBooks(){
-  myLibrary.forEach(buildBookTile);
+ // Adds new Book object to myLibrary array
+function addBookToLibrary(obj){
+  deleteDupes(obj);
+  myLibrary.push(obj);
+};
+
+ // If multiple books sharing the same Title are entered, delete old book
+function deleteDupes(obj){
+  for(let i = 0; i < myLibrary.length; i++){
+    if(obj.title === myLibrary[i].title){
+      myLibrary.splice(i, 1);
+    };
+  }; 
+};
+
+ // Removes Book object from myLibrary array
+function removeBookFromLibrary(obj){
+  myLibrary.pop(obj);
   return;
 };
-  
-btn.addEventListener('click', () =>{
-  addBookToLibrary(data);
+
+ // Switches form's visibility between 'hidden' and 'visibile'
+function toggleForm(){
+  form.reset();
+  form_container.classList.toggle('hidden');
+  return;
+};
+
+ // Clears the existing grid of all elements
+function clearGrid(){
+  let table_divs = document.querySelectorAll('.table-div');
+  table_divs.forEach((element)=>{
+    if(element !== table_divs[0]){
+      element.remove();
+    };
+  }); 
+}; 
+
+ // Runs through myLibrary array and displays information
+function displayLibrary(){
+  clearGrid();
+  myLibrary.forEach((book) => {
+    buildTile(book);
+  });
+};
+
+// Returns the value of the checked radio box
+function radioCheck(){
+  let radios = document.getElementsByName('read');
+  for(let i = 0; i < radios.length; i++){
+    if(radios[i].checked){
+      return radios[i].value;
+    };
+  }; 
+};
+
+ // Creates a book object from form inputs
+function getInfo(obj){
+  let array = [];
+  for(let i = 1; i < obj.length; i++){
+    array.push(obj[i].value);
+  };
+  let book = new Book(array[0], array[1], array[2], radioCheck()) // inputs checked radio input's value
+  return book;
+};
+
+ // Appends an empty title input warning
+function inputTitleErr(){
+  let label = document.querySelector('label[for="title"]')
+  if(label.lastElementChild){return};
+  let p = document.createElement('p');
+  label.appendChild(p).classList.add = 'inputTitleErr_p';
+  p.textContent = '*title is empty';
+};
+
+ // removes the warning label as you type into the input
+function checkWarning(){
+  let label = document.querySelector('label[for="title"]')
+  if(label.lastElementChild){
+    label.lastElementChild.remove();
+  } return;
+}
+
+submit_btn.addEventListener('click', ()=>{
+  let book = getInfo(form.elements);
+  if((book.title === '' || book.title === undefined)){ // checks if title was left empty
+    inputTitleErr();
+    return false; // ensures the form  will not be submitted 
+  } else {
+    addBookToLibrary(book);
+  };
+  toggleForm();
+  displayLibrary();
+  return;
 });
- // // Loops through myLibrary array and displays book-tiles
-myLibrary.forEach(() => console.log('hey'));
+
